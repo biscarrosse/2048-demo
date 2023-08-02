990c009aa54e1ee778a07470d53ecd793c25c567
Ales Klasek <klasek.ales@gmail.com>
2023-08-02 09:14:59 +0200

    feat(app): on merge rm atop tiles

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,35 +1,10 @@
 import { useState, useEffect } from "react";
 import { ACCEPTED_KEYS, CELLS, TILES_INITIAL } from "../constants";
+import { getCollidingTile, removeAtopTiles } from "../utils";
 import { Tile } from "./Tile";
 
 // TODO: TS
 // TODO: func get random tile with value 2 to available position
-
-const getCollidingTile = (nextCoordinates, allCurrentTiles, direction) => {
-  const collidingTile = allCurrentTiles.find(
-    (tile) => tile.x === nextCoordinates.x && tile.y === nextCoordinates.y
-  );
-
-  if (!collidingTile) {
-    return null;
-  } else {
-    // colliding tile can NOT move in the next direction,
-    // hence return the colliding tile:
-    if (direction === "ArrowRight" && collidingTile.x === 3) {
-      return collidingTile;
-    } else if (direction === "ArrowUp" && collidingTile.y === 0) {
-      return collidingTile;
-    } else if (direction === "ArrowLeft" && collidingTile.x === 0) {
-      return collidingTile;
-    } else if (direction === "ArrowDown" && collidingTile.y === 3) {
-      return collidingTile;
-    } else {
-      // colliding tile can move in the next direction,
-      // hence return null, no collision
-      return null;
-    }
-  }
-};
 
 const Game = () => {
   const [tiles, setTiles] = useState(TILES_INITIAL);
@@ -44,52 +19,71 @@ const Game = () => {
     setTiles((prev) => {
       const nextTiles = prev.map((prevTile) => {
         let nextTile = Object.assign({}, prevTile);
+        let nextCoordinates = {
+          x: null,
+          y: null,
+        };
 
         if (direction === "ArrowRight") {
-          let nextCoordinates = {
+          nextCoordinates = {
             x: nextTile.x === 3 ? 3 : nextTile.x + 1,
             y: nextTile.y,
           };
-
-          const collidingTile = getCollidingTile(
-            nextCoordinates,
-            prev,
-            direction
-          );
-
-          if (collidingTile && nextTile.value === collidingTile.value) {
-            console.log("___ merge & sum two tiles onto next position: ");
-            console.log("___ tile that moves: ", prevTile);
-            console.log("___ collidingTile: ", collidingTile);
-
-            nextTile.value = 2 * nextTile.value;
-            // make it a single tile, instead of two atop each other
-          }
-          nextTile.x = nextCoordinates.x;
-          nextTile.y = nextCoordinates.y;
         }
         if (direction === "ArrowUp") {
-          nextTile.y = nextTile.y === 0 ? 0 : nextTile.y - 1;
+          nextCoordinates = {
+            x: nextTile.x,
+            y: nextTile.y === 0 ? 0 : nextTile.y - 1,
+          };
         }
         if (direction === "ArrowLeft") {
-          nextTile.x = nextTile.x === 0 ? 0 : nextTile.x - 1;
+          nextCoordinates = {
+            x: nextTile.x === 0 ? 0 : nextTile.x - 1,
+            y: nextTile.y,
+          };
         }
         if (direction === "ArrowDown") {
-          nextTile.y = nextTile.y === 3 ? 3 : nextTile.y + 1;
+          nextCoordinates = {
+            x: nextTile.x,
+            y: nextTile.y === 3 ? 3 : nextTile.y + 1,
+          };
         }
+        const collidingTile = getCollidingTile(
+          nextCoordinates,
+          prev,
+          direction
+        );
+
+        if (collidingTile && nextTile.value === collidingTile.value) {
+          // merge two tiles into one
+          // double the value & remove the colliding one from tiles state
+
+          nextTile.value = 2 * nextTile.value; // FIXME: if last one touches end and there is no second one, do no double on that ocassion
+
+          // TODO: now make it a single tile,
+          // instead of two atop each other
+          // just remove the one who was colliding
+        }
+        nextTile.x = nextCoordinates.x;
+        nextTile.y = nextCoordinates.y;
+
+        // TODO: if nextTile.value === 2048, win the game
+
         return nextTile;
       });
 
-      return nextTiles;
+      const nextTilesWithoutAtopTiles = removeAtopTiles(nextTiles);
+      return nextTilesWithoutAtopTiles;
     });
+
+    // TODO: right after setting new tiles,
+    // generate a new one with value of 2
   };
 
   useEffect(() => {
     window.addEventListener("keydown", handleEvent);
-
     return () => {
       // cleanup
-
       window.removeEventListener("keydown", handleEvent);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export const isDivisibleByFour = (num) => {
+const isDivisibleByFour = (num) => {
   const input = typeof num === "number" ? num.toString() : num;
 
   if (input === "0") return false;
@@ -9,3 +9,46 @@ export const isDivisibleByFour = (num) => {
   // yet using Number constructor I make myself clear about my intention
   return !Boolean(Number(lastTwoCharacters) % 4);
 };
+
+const getCollidingTile = (nextCoordinates, allCurrentTiles, direction) => {
+  const collidingTile = allCurrentTiles.find(
+    (tile) => tile.x === nextCoordinates.x && tile.y === nextCoordinates.y
+  );
+
+  if (!collidingTile) {
+    return null;
+  } else {
+    // colliding tile can NOT move in the next direction,
+    // hence return the colliding tile:
+    if (direction === "ArrowRight" && collidingTile.x === 3) {
+      return collidingTile;
+    } else if (direction === "ArrowUp" && collidingTile.y === 0) {
+      return collidingTile;
+    } else if (direction === "ArrowLeft" && collidingTile.x === 0) {
+      return collidingTile;
+    } else if (direction === "ArrowDown" && collidingTile.y === 3) {
+      return collidingTile;
+    } else {
+      // colliding tile can move in the next direction,
+      // hence return null, no collision
+      return null;
+    }
+  }
+};
+
+const removeAtopTiles = (tiles) => {
+  const accumulated = [];
+  tiles.forEach((tile) => {
+    if (!accumulated.length) {
+      accumulated.push(tile);
+    }
+    const found = accumulated.find((a) => a.x === tile.x && a.y === tile.y);
+    if (!found) {
+      accumulated.push(tile);
+    }
+  });
+
+  return accumulated;
+};
+
+export { getCollidingTile, isDivisibleByFour, removeAtopTiles };
