c62c106358aba370881f0b9be54938c53e0e7c3a
Ales Klasek <klasek.ales@gmail.com>
2023-08-03 14:04:13 +0200

    feat(app): grahpql client setup example

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,25 @@
+// import { GET_DATA } from "../../index";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql,
+// } from "@apollo/client";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  // GraphQL example:
+  //
+  // const { loading, error, data } = useQuery(GET_DATA);
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error : {error.message}</p>;
+  // return data.locations.map(({ id, name photo }) => (
+  //   <div key={id}>
+  //     <h1>{name}</h1>
+  //     <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+  //   </div>
+  // ));
+
   return (
     <div className="wrap">
       <h1>2048</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,44 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Game from "./components/Game/Game";
 import Layout from "./components/Layout/Layout";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  //useQuery,
+  gql,
+} from "@apollo/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const client = new ApolloClient({
+  uri: "https://need-a-working-backend-api-here.com/graphiql",
+  // FIXME: no graphiql there
+  // assuming this API would work, I could access provided data via
+  // GET_DATA query - example below
+  // Also see usage as presented in "src/components/Layout/Layout.tsx"
+  cache: new InMemoryCache(),
+});
+
+export const GET_DATA = gql`
+  query GetData {
+    exampleKey {
+      id
+      exampleNestedKey
+      exampleNestedKey2
+      exampleNestedKey3
+    }
+  }
+`;
+
 root.render(
   <React.StrictMode>
-    <Layout>
-      <Game />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Game />
+      </Layout>
+    </ApolloProvider>
   </React.StrictMode>
 );
